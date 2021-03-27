c80322e3c3901cdae5c314667c28df77d42fe087
Crystal <crystal.zhouxiaoyue@hotmail.com>
2021-03-27 16:19:43 +0800

    Add EJS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes/index.js');
+var path = require('path');
 var port = process.env.PORT || 8080;
 
 app.use('/', routes);
@@ -17,3 +18,15 @@ app.listen(port, function(err) {
   if (err) throw err;
   console.log(`app running at: ${port}`);
 });
+
+app.set('views', path.join(__dirname, 'public'));
+
+var contactList = [{
+    name: 'Bob',
+    age: 12
+  },
+  {
+    name: 'Anton',
+    age: 30
+  }
+];
