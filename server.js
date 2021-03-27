9017c329b9c2598b97496e2d425c0d56227e4454
Crystal <crystal.zhouxiaoyue@hotmail.com>
2021-03-27 16:10:27 +0800

    Update Structure

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var app = express();
-
-// set port
+var routes = require('./routes/index.js');
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.use('/', routes);
+
+// app.use(express.static(__dirname));
 
 // routes
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
   res.render("index");
-});
+});*/
 
-app.listen(port, function() {
-  console.log("app running");
+app.listen(port, function(err) {
+  if (err) throw err;
+  console.log(`app running at: ${port}`);
 });
